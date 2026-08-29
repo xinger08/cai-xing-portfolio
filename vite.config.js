8145a528932fa9197401ba485dc222c9dcdf64ef
xinger08 <1316696946@qq.com>
2026-08-29 17:09:01 +0800

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/cai-xing-portfolio/',
   server: {
     watch: {
       ignored: ['**/work/**', '**/qa/**', '**/dist/**'],
